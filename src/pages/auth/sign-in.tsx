bcca420dd6c594d67da4e36c15bf37bf1280f59f
Almir Jr <99154364+AlmirJrDev@users.noreply.github.com>
2024-02-09 08:11:11 -0300

    error: themeprovider

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -15,14 +17,33 @@ export function SignIn() {
   const { register, handleSubmit, formState:{isSubmitting}} = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
+   try {
     console.log(data)
+
+ 
+
     await new Promise(resolve => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autenticação para seu email.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => {handleSignIn(data)}
+      }
+    })
+   } catch {
+    toast.error('Credenciais inválidas')
+   }
  }
   return (
     <>
       <Helmet title="Login" />
 
       <div className="p-8">
+         <Button variant='ghost' asChild className='absolute right-4 top-8'>
+        <Link to="/sign-up" className=''>
+          Novo estabelecimento 
+        </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layout/app'
 import { AuthLayout } from './pages/_layout/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -13,8 +14,12 @@ export const router = createBrowserRouter([
   },
 
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> }
+  
+  ],
   },
 ])
